caad8a80bf8388a0c6b63c99167681b0ee3101ca
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2023-10-26 15:59:45 +0530

    feat: moving wave animation
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- tcp-server/public/assets/js/main.js
+++ tcp-server/public/assets/js/main.js
@@ -4,9 +4,15 @@ const TOTAL_CELL = W * H;
 
 const fract = (x) => x - Math.floor(x);
 
-const draw = (ctx, _t) => {
+const pos = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+const vel = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1];
+// const vel = [1, 2, 1, 3, 1, 4, 1, 5, 1, 6, 1, 7, 1];
+
+const draw = (ctx, _t, _delta_t) => {
     // const t = _t * 0.001;
+    // const t = _t * 0.0001;
     const t = _t * 0.0001;
+    const delta_t = _delta_t * 0.01;
     ctx.save();
     // for (let r = 0; r < H; r++) {
     //     for (let c = 0; c < W; c++) {
@@ -18,12 +24,31 @@ const draw = (ctx, _t) => {
     // }
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, W, H);
-    const tt1 = fract(t / 3);
-    ctx.fillStyle = tt1 > 0.6 ? '#0000ff' : (tt1 > 0.3 ? '#00ff00' : '#ff0000');
-    ctx.font = "13px serif";
-    const text = "Happy Dussehra!!";
-    const text_len = text.length * 6;
-    ctx.fillText(text, 10 + fract(t) * -text_len, 10);
+    for (let col = 0; col < 13; col++) {
+        pos[col] = pos[col] + vel[col] * delta_t;
+        if (pos[col] < 0) {
+            pos[col] = 0;
+            vel[col] *= -1;
+        } else if (pos[col] >= 13) {
+            pos[col] = 12;
+            vel[col] *= -1;
+        }
+        // ctx.fillStyle = 'green';
+        ctx.fillStyle = `hsl(${360 * col / 13}deg, 100%, 50%)`;
+        ctx.fillRect(col, Math.floor(pos[col]), 1, 1);
+        // for (let row = 0; row < 13; row += 8) {
+        //     const rr = Math.floor(pos[col]);
+        //     const rc = (rr + row) % 13;
+        //     ctx.fillRect(col, rc, 1, 1);
+        // }
+    }
+
+    // const tt1 = fract(t / 3);
+    // ctx.fillStyle = tt1 > 0.6 ? '#0000ff' : (tt1 > 0.3 ? '#00ff00' : '#ff0000');
+    // ctx.font = "13px serif";
+    // const text = "Happy Dussehra!!";
+    // const text_len = text.length * 6;
+    // ctx.fillText(text, 10 + fract(t) * -text_len, 10);
     ctx.restore();
 };
 
@@ -57,9 +82,10 @@ const main = () => {
         let last_t = 0;
         const step = (t) => {
             requestAnimationFrame(step);
-            if (t - last_t < 1) return;
+            const delta_t = t - last_t;
+            if (delta_t < 1) return;
             last_t = t;
-            draw(ctx, t);
+            draw(ctx, t, delta_t);
             const imgData = ctx.getImageData(0, 0, W, H);
             // console.log('imgData:', imgData);
             ws.send(imgData.data);
