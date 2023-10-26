f70b19bc76fbb3b3914c958a385ab44e4cc52bd4
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2023-10-26 22:48:31 +0530

    feat: zooming text
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- tcp-server/public/assets/js/main.js
+++ tcp-server/public/assets/js/main.js
@@ -24,24 +24,32 @@ const draw = (ctx, _t, _delta_t) => {
     // }
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, W, H);
-    for (let col = 0; col < 13; col++) {
-        pos[col] = pos[col] + vel[col] * delta_t;
-        if (pos[col] < 0) {
-            pos[col] = 0;
-            vel[col] *= -1;
-        } else if (pos[col] >= 13) {
-            pos[col] = 12;
-            vel[col] *= -1;
-        }
-        // ctx.fillStyle = 'green';
-        ctx.fillStyle = `hsl(${360 * col / 13}deg, 100%, 50%)`;
-        ctx.fillRect(col, Math.floor(pos[col]), 1, 1);
-        // for (let row = 0; row < 13; row += 8) {
-        //     const rr = Math.floor(pos[col]);
-        //     const rc = (rr + row) % 13;
-        //     ctx.fillRect(col, rc, 1, 1);
-        // }
-    }
+    ctx.fillStyle = `hsl(${360 * fract(t)}deg, 100%, 50%)`;
+    const font_size = 10 + fract(t) * 10;
+    ctx.font = `${font_size}px serif`;
+    ctx.fillText('A', 0, 12);
+    // for (let font_size = 20; font_size > 10; font_size--) {
+    //     ctx.fillStyle = `hsl(${360 * font_size / 13}deg, 100%, 50%)`;
+    //     ctx.font = `${font_size}px serif`;
+    //     ctx.fillText('A', 0, 12);
+    // }
+    // for (let col = 0; col < 13; col++) {
+    //     pos[col] = pos[col] + vel[col] * delta_t;
+    //     if (pos[col] < 0) {
+    //         pos[col] = 0;
+    //         vel[col] *= -1;
+    //     } else if (pos[col] >= 13) {
+    //         pos[col] = 12;
+    //         vel[col] *= -1;
+    //     }
+    //     // ctx.fillStyle = 'green';
+    //     // ctx.fillRect(col, Math.floor(pos[col]), 1, 1);
+    //     // for (let row = 0; row < 13; row += 8) {
+    //     //     const rr = Math.floor(pos[col]);
+    //     //     const rc = (rr + row) % 13;
+    //     //     ctx.fillRect(col, rc, 1, 1);
+    //     // }
+    // }
 
     // const tt1 = fract(t / 3);
     // ctx.fillStyle = tt1 > 0.6 ? '#0000ff' : (tt1 > 0.3 ? '#00ff00' : '#ff0000');
